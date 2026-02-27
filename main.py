3064f5c335d59f59d603d6628d0c3da3d63474d1
wcxt <68016750+wcxt@users.noreply.github.com>
2026-02-27 16:49:26 +0100

    Add basic link shortening api

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
+urls = {}
 app = FastAPI()
+
+class CodeCreate(BaseModel):
+    url: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/code")
+def shorten_url(body: CodeCreate):
+    code_id = len(urls) + 1
+    urls[code_id] = body.url
+    return {"id": code_id}
+
+@app.get("/code/{code_id}")
+def read_code(code_id: int):
+    if code_id not in urls:
+        raise HTTPException(status_code=404, detail="Code not found")
+
+    return {"url": urls[code_id]}
