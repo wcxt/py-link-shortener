d734685fce3a25b29a996c1e300ab0a39ea65bcd
wcxt <68016750+wcxt@users.noreply.github.com>
2026-02-28 14:09:50 +0100

    Add url redirections

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
 urls = {}
@@ -11,15 +12,15 @@ class CodeCreate(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/{code_id}")
+def redirect_code(code_id: int):
+    if code_id not in urls:
+        raise HTTPException(status_code=404, detail="Code not found")
+
+    return RedirectResponse(urls[code_id], status_code=301)
+
 @app.post("/code")
 def shorten_url(body: CodeCreate):
     code_id = len(urls) + 1
     urls[code_id] = body.url
     return {"id": code_id}
-
-@app.get("/code/{code_id}")
-def read_code(code_id: int):
-    if code_id not in urls:
-        raise HTTPException(status_code=404, detail="Code not found")
-
-    return {"url": urls[code_id]}
