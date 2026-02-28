9cae50114f4273911187a8b3f8f8ebfaf299a46b
wcxt <68016750+wcxt@users.noreply.github.com>
2026-02-28 15:38:55 +0100

    Add index page

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+from fastapi import FastAPI, Form, HTTPException, Request
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel, HttpUrl
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, HttpUrl, ValidationError
 
 urls = {}
 app = FastAPI()
 
-class CodeCreate(BaseModel):
+templates = Jinja2Templates(directory="templates")
+
+class ShortenFormData(BaseModel):
     url: HttpUrl
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/{code_id}")
 def redirect_code(code_id: int):
@@ -20,7 +24,12 @@ def redirect_code(code_id: int):
     return RedirectResponse(urls[code_id], status_code=301)
 
 @app.post("/code")
-def shorten_url(body: CodeCreate):
-    code_id = len(urls) + 1
-    urls[code_id] = body.url
-    return {"id": code_id}
+def shorten_url(request: Request, url: Annotated[str, Form()]):
+    try:
+        data = ShortenFormData(url=HttpUrl(url))
+        code_id = len(urls) + 1
+        urls[code_id] = data.url
+        return {"id": code_id}
+    except ValidationError:
+        return templates.TemplateResponse(request=request, name="index.html",
+                                          context={"error": "Invalid URL. Please enter a valid URL."})
