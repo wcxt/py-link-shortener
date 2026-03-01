6ef97b327c9887290a02166b4a1758370cb7d720
wcxt <68016750+wcxt@users.noreply.github.com>
2026-03-01 19:15:25 +0100

    Add correct response status codes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-from fastapi import FastAPI, Form, HTTPException, Request
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Form, HTTPException, Request, status
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, HttpUrl, ValidationError
 
@@ -12,25 +12,29 @@ templates = Jinja2Templates(directory="templates")
 class ShortenFormData(BaseModel):
     url: HttpUrl
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
-@app.get("/{code_id}")
-def redirect_code(code_id: int):
-    if code_id not in urls:
-        raise HTTPException(status_code=404, detail="Code not found")
-
-    return RedirectResponse(urls[code_id], status_code=301)
-
-@app.post("/code")
+@app.post("/", response_class=HTMLResponse)
 def shorten_url(request: Request, url: Annotated[str, Form()]):
     try:
         data = ShortenFormData(url=HttpUrl(url))
         code_id = len(urls) + 1
         urls[code_id] = data.url
         return templates.TemplateResponse(request=request, name="code.html",
-                                          context={"code_id": code_id})
+                                          context={"code_id": code_id},
+                                          status_code=status.HTTP_201_CREATED)
     except ValidationError:
         return templates.TemplateResponse(request=request, name="index.html",
-                                          context={"error": "Invalid URL. Please enter a valid URL."})
+                                          context={"error": "Invalid URL. Please enter a valid URL."},
+                                          status_code=status.HTTP_422_UNPROCESSABLE_CONTENT)
+
+@app.get("/{code_id}", response_class=RedirectResponse, status_code=status.HTTP_301_MOVED_PERMANENTLY)
+def redirect_code(code_id: int):
+    if code_id not in urls:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Code not found")
+
+    return urls[code_id]
+
+
