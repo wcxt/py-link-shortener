2784487eb6de1f460f2baae54f8dd79fb212b800
wcxt <68016750+wcxt@users.noreply.github.com>
2026-03-25 14:57:21 +0100

    Add javascript based data fetching

--- static/index.js
+++ static/index.js
@@ -1,1 +1,38 @@
-console.log("This works :D")
+const form = document.getElementById("shorten-form");
+const errorEl = document.getElementById("error");
+const resultEl = document.getElementById("result");
+
+form.addEventListener("submit", async (e) => {
+	e.preventDefault()
+
+	errorEl.textContent = ""
+	resultEl.textContent = ""
+
+	const url = document.getElementById("url").value
+
+	try {
+		const res = await fetch("/api/short", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ url })
+		})
+
+		const data = await res.json()
+
+		if (!res.ok) {
+			if (res.status == 422 && data.detail) {
+				errorEl.textContent = data.detail[0].msg
+				return
+			} else if (res.status == 500 && data.detail) {
+				errorEl.textContent = data.detail
+				return
+			}
+			throw new Error("Something went wrong")
+		}
+		const short_url = `${window.location.origin}/${data.short_code}`
+		resultEl.innerHTML = `Short URL: <a href="${short_url}" target="_blank">${short_url}</a>`
+		form.reset()
+	} catch (error) {
+		errorEl.textContent = error.message
+	}
+})
