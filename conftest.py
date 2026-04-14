9b122e708612dda985763c19e79ed046522ee35b
wcxt <68016750+wcxt@users.noreply.github.com>
2026-04-14 10:33:52 +0200

    Add pytest fixtures

---
+++ conftest.py
@@ -0,0 +1,40 @@
+from fastapi.testclient import TestClient
+from sqlmodel import SQLModel, Session, create_engine
+import pytest
+from database import get_session
+from main import app
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/link_shortener_test")
+
+@pytest.fixture(scope="session", autouse=True)
+def db_fixture():
+    SQLModel.metadata.create_all(engine)
+    yield
+    SQLModel.metadata.drop_all(engine)
+
+@pytest.fixture(name="session")
+def session_fixture():
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(bind=connection)
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():  
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+    client = TestClient(app)
+    
+    yield client
+
+    app.dependency_overrides.clear()
+
+    
+
