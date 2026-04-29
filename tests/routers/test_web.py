7dfbcd2dba9ff52616ab75b25789c6b8ce82eb89
wcxt <68016750+wcxt@users.noreply.github.com>
2026-04-29 15:38:35 +0200

    Add user dashboard

--- tests/routers/test_web.py
+++ tests/routers/test_web.py
@@ -1,8 +1,9 @@
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
-from app.models import ShortenedURL
-from tests.conftest import TEST_URL
+from app.core.security import get_hashed_password
+from app.models import ShortenedURL, User
+from tests.conftest import TEST_EMAIL, TEST_PASSWORD, TEST_URL
 
 
 def test_redirect_code(session: Session, client: TestClient):
@@ -39,3 +40,30 @@ def test_read_login(client: TestClient):
 
     assert response.status_code == 200 
     assert "text/html" in response.headers["content-type"]
+
+def test_read_dashboard(session: Session, client: TestClient):
+    test_user = User(email=TEST_EMAIL, password_hash=get_hashed_password(TEST_PASSWORD))
+    session.add(test_user)
+    session.commit()
+    session.refresh(test_user)
+
+    response = client.post(
+            "/api/token",
+            data={"grant_type": "password", "username": TEST_EMAIL, "password": TEST_PASSWORD}
+    )
+    data = response.json()
+    access_token = data["access_token"]
+
+    response = client.get("/dashboard", headers={"Authorization": f"Bearer {access_token}"})
+
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+def test_read_dashboard_unauthenticated(client: TestClient):
+    response = client.get("/dashboard")
+
+    data = response.json()
+
+    assert response.status_code == 401
+    assert response.headers["WWW-Authenticate"] == "Bearer"
+    assert data["detail"] is not None
