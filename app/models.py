eb808b21144f218bdd6e1766f1ead119a0beda50
wcxt <68016750+wcxt@users.noreply.github.com>
2026-05-14 20:01:42 +0200

    Change expired_at type

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from nanoid import generate
+from sqlalchemy import Column
 from sqlmodel import Field, Relationship, SQLModel, DateTime
 
 CODE_SIZE=8
@@ -20,7 +21,7 @@ class ShortenedURL(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     code: str = Field(default_factory=generate_code, max_length=CODE_SIZE, unique=True, index=True)
     url: str = Field(max_length=2048)
-    expires_at: datetime | None = Field(default=None)
+    expires_at: datetime | None = Field(default=None, sa_column=Column(DateTime(timezone=True)))
 
     owner_id: int | None = Field(default=None, foreign_key="user.id")
     owner: User | None = Relationship(back_populates="shortened_urls")
